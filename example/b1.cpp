359b48e005de10ac5952f12dc49e76f09074e146
Tianjiao Yin <ytj000@gmail.com>
2012-01-28 06:30:38 +0800

    silly bug: macro name conflict

--- example/b1.cpp
+++ example/b1.cpp
@@ -1,12 +1,13 @@
 #include"../include/acml/extension/boost/variant.hpp"
-#include"../include/acml/json.hpp"
+#include"../include/acml/extension/boost/optional.hpp"
+#include"../include/acml/xml.hpp"
 #include<string>
 #include<iostream>
 using namespace std;
 
-boost::variant<int, string, double> s;
+boost::optional< boost::variant<int, string, double> > s;
 
 int main() {
-    s = "3.14";
-    cout << acml::json::dumps(s) << endl;    
+    s = string("hello world");
+    cout << acml::xml::dumps(s) << endl;    
 }
