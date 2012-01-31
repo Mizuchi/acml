e58a52b44b7f517fa6d736befb64dee17a47632f
Tianjiao Yin <ytj000@gmail.com>
2012-01-31 13:49:42 +0800

    fixed no type_name in ACML_REGISTER_TEMPLATE

--- include/acml/acml.hpp
+++ include/acml/acml.hpp
@@ -46,6 +46,7 @@ void for_each(const Value& value, const Visitor& visitor) {
     struct Reflector< TYPE<BOOST_PP_ENUM_PARAMS(LENGTH, T)> > {             \
         typedef TYPE<BOOST_PP_ENUM_PARAMS(LENGTH, T)> Value;                \
         static const bool defined = true;                                   \
+        static const char* type_name() { return #TYPE; }                    \
         template<class Visitor>                                             \
         static void for_each(const Value&, const Visitor&);                 \
     }; } /* namespace acml */                                               \
